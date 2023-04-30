23e96b271b0e7d06ce5adb038fda11b6e6636f91
Jermatria Keating <Jeremy.w.keating@gmail.com>
2023-04-30 21:44:26 +1200

    Add project files.

---
+++ Animations.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using ItemManager;
+using UnityEngine;
+
+
+namespace PungusSoulsAnimations
+{
+    [HarmonyPatch]
+    public class PungusAnimations
+    {
+        private static readonly Dictionary<string, string> CoolAnimation = new();
+        private static Dictionary<string, AnimationClip> _externalAnimations = new();
+        private static bool _firstInit;
+        internal static RuntimeAnimatorController? MyNewAnimation;
+        internal static RuntimeAnimatorController? OrigAnimation;
+        internal static void AnimationAwake()
+        {
+            AssetBundle asset = PrefabManager.RegisterAssetBundle("animations", "assets");
+            CoolAnimation.Add("Dance", "MyCoolDance1");
+            CoolAnimation.Add("Greatsword Secondary Attack", "GreatSwordSlashNew");
+
+            _externalAnimations.Add("MyCoolDance1", asset.LoadAsset<AnimationClip>("MyCoolDance1"));
+            _externalAnimations.Add("GreatSwordSlashNew", asset.LoadAsset<AnimationClip>("GreatSwordSlashNew"));
+        }
+        private static RuntimeAnimatorController MakeAoc(IReadOnlyDictionary<string, string> replacement,
+            RuntimeAnimatorController original)
+        {
+            AnimatorOverrideController aoc = new(original);
+            List<KeyValuePair<AnimationClip, AnimationClip>> anims = new();
+            foreach (AnimationClip animation in aoc.animationClips)
+            {
+                string name = animation.name;
+                if (replacement.ContainsKey(name))
+                {
+                    AnimationClip newClip = Object.Instantiate(_externalAnimations[replacement[name]]);
+                    anims.Add(new KeyValuePair<AnimationClip, AnimationClip>(animation, newClip));
+                }
+                else
+                {
+                    anims.Add(new KeyValuePair<AnimationClip, AnimationClip>(animation, animation));
+                }
+            }
+
+            aoc.ApplyOverrides(anims);
+            return aoc;
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.Start))]
+        [HarmonyPriority(Priority.Last)]
+        private static class TESTPATCHPLAYERANIMS
+        {
+            private static void Postfix(Player __instance)
+            {
+                if (_firstInit) return;
+                _firstInit = true;
+
+                OrigAnimation = MakeAoc(new Dictionary<string, string>(),
+                    __instance.m_animator.runtimeAnimatorController);
+                MyNewAnimation = MakeAoc(CoolAnimation, __instance.m_animator.runtimeAnimatorController);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using CreatureManager;
+using HarmonyLib;
+using ItemManager;
+using JetBrains.Annotations;
+using LocalizationManager;
+using LocationManager;
+using PieceManager;
+using ServerSync;
+using SkillManager;
+using StatusEffectManager;
+using UnityEngine;
+using PrefabManager = ItemManager.PrefabManager;
+using Range = LocationManager.Range;
+
+namespace PungusSoulsAnimations
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class PungusSoulsAnimationsPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "PungusSouls_Animations";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Pungus";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+        public static PungusSoulsAnimationsPlugin context;
+
+        public static readonly ManualLogSource PungusSoulsAnimationsLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+        { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        // Location Manager variables
+        public Texture2D tex;
+        private Sprite mySprite;
+        private SpriteRenderer sr;
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            context = this;          
+            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On,
+                "If on, the configuration is locked and can be changed by server admins only.");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            /*Assembly assembly = Assembly.GetExecutingAssembly();*/
+            PungusAnimations.AnimationAwake();
+            _harmony.PatchAll();
+            SetupWatcher();
+        }
+        public void update()
+        {
+            if (!Player.m_localPlayer) return;
+            {
+                Player.m_localPlayer.m_animator.runtimeAnimatorController = PungusAnimations.MyNewAnimation;
+            }
+        }
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                PungusSoulsAnimationsLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                PungusSoulsAnimationsLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                PungusSoulsAnimationsLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            [UsedImplicitly] public int? Order;
+            [UsedImplicitly] public bool? Browsable;
+            [UsedImplicitly] public string? Category;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer;
+        }
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+        }
+
+        #endregion
+    }
+}
